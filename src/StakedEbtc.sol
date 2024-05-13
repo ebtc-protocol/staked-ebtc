a72a60757e942040023ff251dc27d3fbfada50fa
wtj2021 <wtj2022@proton.me>
2024-05-13 01:20:11 -0700

    prototype

---
+++ src/StakedEbtc.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.20;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { SafeCastLib } from "@solmate/utils/SafeCastLib.sol";
+import { SafeTransferLib } from "@solmate/utils/SafeTransferLib.sol";
+import { LinearRewardsErc4626, ERC20 } from "./LinearRewardsErc4626.sol";
+import { AuthNoOwner } from "./AuthNoOwner.sol";
+
+/// @title Staked eBTC
+/// @notice A ERC4626 Vault implementation with linear rewards, rewards can be capped
+contract StakedEBtc is LinearRewardsErc4626, AuthNoOwner {
+    using SafeTransferLib for ERC20;
+    using SafeCastLib for *;
+
+    /// @notice The maximum amount of rewards that can be distributed per second per 1e18 asset
+    uint256 public maxDistributionPerSecondPerAsset;
+
+    mapping(address => bool) public approvedDonors;
+
+    event Donation(address indexed donor, uint256 amount);
+    event DonorApprovalUpdated(address indexed donor, bool approved);
+
+    /// @notice Receive an eBTC donation from an authorized donor
+    function donate(uint256 amount) external {
+        require(approvedDonors[msg.sender], "StakedEBtc: invalid donor");
+        donationBalance += amount;
+        asset.safeTransferFrom(msg.sender, address(this), amount);
+        emit Donation(msg.sender, amount);
+    }
+
+    function setApprovedDonor(address donor, bool approved) external requiresAuth() {
+        approvedDonors[donor] = approved;
+        emit DonorApprovalUpdated(donor, approved);
+    }
+
+    /// @param _underlying The erc20 asset deposited
+    /// @param _name The name of the vault
+    /// @param _symbol The symbol of the vault
+    /// @param _rewardsCycleLength The length of the rewards cycle in seconds
+    /// @param _maxDistributionPerSecondPerAsset The maximum amount of rewards that can be distributed per second per 1e18 asset
+    constructor(
+        IERC20 _underlying,
+        string memory _name,
+        string memory _symbol,
+        uint32 _rewardsCycleLength,
+        uint256 _maxDistributionPerSecondPerAsset,
+        address _authorityAddress
+    ) LinearRewardsErc4626(ERC20(address(_underlying)), _name, _symbol, _rewardsCycleLength)
+    {
+        maxDistributionPerSecondPerAsset = _maxDistributionPerSecondPerAsset;
+        _initializeAuthority(_authorityAddress);
+    }
+
+    /// @notice The ```SetMaxDistributionPerSecondPerAsset``` event is emitted when the maxDistributionPerSecondPerAsset is set
+    /// @param oldMax The old maxDistributionPerSecondPerAsset value
+    /// @param newMax The new maxDistributionPerSecondPerAsset value
+    event SetMaxDistributionPerSecondPerAsset(uint256 oldMax, uint256 newMax);
+
+    /// @notice The ```setMaxDistributionPerSecondPerAsset``` function sets the maxDistributionPerSecondPerAsset
+    /// @dev This function can only be called by the timelock, caps the value to type(uint64).max
+    /// @param _maxDistributionPerSecondPerAsset The maximum amount of rewards that can be distributed per second per 1e18 asset
+    function setMaxDistributionPerSecondPerAsset(uint256 _maxDistributionPerSecondPerAsset) external requiresAuth {
+        syncRewardsAndDistribution();
+
+        // NOTE: prevents bricking the contract via overflow
+        if (_maxDistributionPerSecondPerAsset > type(uint64).max) {
+            _maxDistributionPerSecondPerAsset = type(uint64).max;
+        }
+
+        emit SetMaxDistributionPerSecondPerAsset({
+            oldMax: maxDistributionPerSecondPerAsset,
+            newMax: _maxDistributionPerSecondPerAsset
+        });
+
+        maxDistributionPerSecondPerAsset = _maxDistributionPerSecondPerAsset;
+    }
+
+    /// @notice The ```calculateRewardsToDistribute``` function calculates the amount of rewards to distribute based on the rewards cycle data and the time passed
+    /// @param _rewardsCycleData The rewards cycle data
+    /// @param _deltaTime The time passed since the last rewards distribution
+    /// @return _rewardToDistribute The amount of rewards to distribute
+    function calculateRewardsToDistribute(
+        RewardsCycleData memory _rewardsCycleData,
+        uint256 _deltaTime
+    ) public view override returns (uint256 _rewardToDistribute) {
+        _rewardToDistribute = super.calculateRewardsToDistribute({
+            _rewardsCycleData: _rewardsCycleData,
+            _deltaTime: _deltaTime
+        });
+
+        // Cap rewards
+        uint256 _maxDistribution = (maxDistributionPerSecondPerAsset * _deltaTime * storedTotalAssets) / PRECISION;
+        if (_rewardToDistribute > _maxDistribution) {
+            _rewardToDistribute = _maxDistribution;
+        }
+    }
+}
